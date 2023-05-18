bd90d0cb104bd8fec9cf47a932b866965c8c5344
cediperez12 <kirisakinoshka@gmail.com>
2023-05-18 16:19:21 +0800

    initial commit

---
+++ src/main/java/com/yondu/knowledgebase/YonduKnowledgebaseApiApplication.java
@@ -0,0 +1,13 @@
+package com.yondu.knowledgebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YonduKnowledgebaseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YonduKnowledgebaseApiApplication.class, args);
+	}
+
+}
