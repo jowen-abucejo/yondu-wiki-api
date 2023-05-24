fbf6fe548cbe77065d11c3e65e38617ce2d1aedb
Jowen Abucejo <jabucejo@yondu.com>
2023-05-24 18:33:06 +0800

    Add page version entity

---
+++ src/main/java/com/yondu/knowledgebase/entities/PageVersion.java
@@ -0,0 +1,106 @@
+package com.yondu.knowledgebase.entities;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.OneToMany;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class PageVersion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Lob
+    private String content;
+
+    @LastModifiedDate
+    private LocalDateTime dateModified;
+
+    @LastModifiedBy
+    private User modifiedBy;
+
+    @OneToMany(mappedBy = "pageVersion")
+    private List<Review> reviews = new ArrayList<>();
+
+    /**
+     * 
+     */
+    public PageVersion() {
+    }
+
+    /**
+     * @param id
+     * @param title
+     * @param content
+     * @param dateModified
+     * @param modifiedBy
+     * @param reviews
+     */
+    public PageVersion(Long id, String title, String content, LocalDateTime dateModified, User modifiedBy,
+            List<Review> reviews) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.dateModified = dateModified;
+        this.modifiedBy = modifiedBy;
+        this.reviews = reviews;
+    }
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @return the content
+     */
+    public String getContent() {
+        return content;
+    }
+
+    /**
+     * @return the dateModified
+     */
+    public LocalDateTime getDateModified() {
+        return dateModified;
+    }
+
+    /**
+     * @return the modifiedBy
+     */
+    public User getModifiedBy() {
+        return modifiedBy;
+    }
+
+    /**
+     * @return the reviews
+     */
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+}
