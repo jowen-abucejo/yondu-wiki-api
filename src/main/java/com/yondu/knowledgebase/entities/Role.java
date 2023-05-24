9c7e6b8995f465df28b71130a8ddfd5053fbd715
Lance Zamora <zamoralance05@gmail.com>
2023-05-24 17:38:30 +0800

    role, Role permission, user permission entity

---
+++ src/main/java/com/yondu/knowledgebase/entities/Role.java
@@ -0,0 +1,49 @@
+package com.yondu.knowledgebase.entities;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @OneToMany(mappedBy = "role", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<RolePermission> rolePermissions = new HashSet<>();
+
+    public Role() {
+    }
+
+    public Role(Long id, String name, Set<RolePermission> rolePermissions) {
+        this.id = id;
+        this.name = name;
+        this.rolePermissions = rolePermissions;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Set<RolePermission> getRolePermissions() {
+        return rolePermissions;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", rolePermissions=" + rolePermissions +
+                '}';
+    }
+
+}
