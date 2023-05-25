6e1a5e7c86192f21483d7e2998649eeba0a4f43a
michael-marzan <michaelmarzan0424@gmail.com>
2023-05-25 14:05:57 +0800

    Category Many-to-Many

--- src/main/java/com/yondu/knowledgebase/entities/Category.java
+++ src/main/java/com/yondu/knowledgebase/entities/Category.java
@@ -2,29 +2,38 @@ package com.yondu.knowledgebase.entities;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 
 @Entity
 public class Category {
+
+   
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
-    @OneToMany(mappedBy ="category")
-    private List<PageCategory> pageCategory = new ArrayList<>();
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "page_category", joinColumns = @JoinColumn(name = "category_id"), inverseJoinColumns =@JoinColumn (name = "page_id"))
+    private List<Page> pages = new ArrayList<>();
 
     public Category(){
 
     }
 
-    public Category(Long id, String name){
+    public Category(Long id, String name, List<Page> pages){
         this.id= id;
         this.name = name;
+        this.page = pages;
     }
 
     public Long getId(){
@@ -33,5 +42,9 @@ public class Category {
 
     public String getName(){
         return name; 
+    }
+
+    public List<Page> getPages(){
+        return pages;
     }
 }
