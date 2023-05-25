d3f06da36aba8306f37412beda8ca598600f3b4e
mjinayon <mjinayon@yondu.com>
2023-05-25 13:19:34 +0800

    minor changes in directory permission

--- src/main/java/com/yondu/knowledgebase/entities/DirectoryPermission.java
+++ src/main/java/com/yondu/knowledgebase/entities/DirectoryPermission.java
@@ -15,6 +15,13 @@ public class DirectoryPermission {
     @Column(nullable = false)
     private String description;
 
+    public DirectoryPermission() {}
+
+    public DirectoryPermission(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
     public Long getId() {
         return id;
     }
