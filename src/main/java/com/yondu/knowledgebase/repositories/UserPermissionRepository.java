5e5ea261c36364db1b4a9ec15151f4d011aca707
Lance Zamora <zamoralance05@gmail.com>
2023-05-25 13:57:51 +0800

    Create user permission controller, repository, and service

---
+++ src/main/java/com/yondu/knowledgebase/repositories/UserPermissionRepository.java
@@ -0,0 +1,9 @@
+package com.yondu.knowledgebase.repositories;
+
+import com.yondu.knowledgebase.entities.UserPermission;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserPermissionRepository extends JpaRepository<UserPermission, Long> {
+}
