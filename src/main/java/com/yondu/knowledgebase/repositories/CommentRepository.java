7b68653c88fc737aa627985f4f7c2c8078fb5934
YONDU\rasistores <rasistores>
2023-05-30 18:16:45 +0800

    comment-refactor: aligned page

--- src/main/java/com/yondu/knowledgebase/repositories/CommentRepository.java
+++ src/main/java/com/yondu/knowledgebase/repositories/CommentRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findAllByParentCommentId(Long parentCommentId);
+    List<Comment> findAllByParentCommentIdAndPageId(Long parentCommentId, Long page_id);
+    List<Comment> findAllByParentCommentIdIsNullAndPageId(Long page_id);
 }
--- src/main/java/com/yondu/knowledgebase/services/CommentService.java
+++ src/main/java/com/yondu/knowledgebase/services/CommentService.java
@@ -1,14 +1,13 @@
 package com.yondu.knowledgebase.services;
 
-import com.yondu.knowledgebase.DTO.Comment.CommentRequestDTO;
+import com.yondu.knowledgebase.DTO.Comment.CommentDTO;
 import com.yondu.knowledgebase.DTO.Comment.CommentResponseDTO;
 import com.yondu.knowledgebase.entities.Comment;
-import org.springframework.http.ResponseEntity;
 
 
 public interface CommentService {
-    public Comment createComment(CommentRequestDTO comment, Long userId, Long commentParentId);
-    public CommentResponseDTO getAllComments();
+    public Comment createComment(CommentDTO commentRequestDTO, Long commentParentId);
+    public CommentResponseDTO getAllComments(Long pageId);
     public int getTotalComments();
     Comment getComment(Long commentId);
 }
