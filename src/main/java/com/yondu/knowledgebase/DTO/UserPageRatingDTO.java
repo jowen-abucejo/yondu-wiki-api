ccc96119e99c760ea9175fd08a2d12548a41ad20
zimoe23 <delapazramir@gmail.com>
2023-05-30 14:00:00 +0800

    Added DTO, Endpoint, Validation.

---
+++ src/main/java/com/yondu/knowledgebase/DTO/UserPageRatingDTO.java
@@ -0,0 +1,39 @@
+package com.yondu.knowledgebase.DTO;
+
+public class UserPageRatingDTO {
+	private String rating;
+	private Long userId;
+	private Long pageId;
+	
+	public UserPageRatingDTO() {}
+	
+	public UserPageRatingDTO(String rating, Long userId, Long pageId) {
+		this.rating = rating;
+		this.userId = userId;
+		this.pageId = pageId;
+	}
+
+	public String getRating() {
+		return rating;
+	}
+
+	public void setRating(String rating) {
+		this.rating = rating;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public Long getPageId() {
+		return pageId;
+	}
+
+	public void setPageId(Long pageId) {
+		this.pageId = pageId;
+	}
+}
---
+++ src/main/java/com/yondu/knowledgebase/services/UserPageRatingService.java
@@ -0,0 +1,64 @@
+package com.yondu.knowledgebase.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import com.yondu.knowledgebase.DTO.UserPageRatingDTO;
+import com.yondu.knowledgebase.entities.Page;
+import com.yondu.knowledgebase.entities.User;
+import com.yondu.knowledgebase.entities.UserPageRating;
+import com.yondu.knowledgebase.repositories.PageRepository;
+import com.yondu.knowledgebase.repositories.UserPageRatingRepository;
+import com.yondu.knowledgebase.repositories.UserRepository;
+
+@Service
+public class UserPageRatingService {
+
+	@Autowired
+	private UserPageRatingRepository userPageRatingRepository;
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private PageRepository pageRepository;
+	
+	public ResponseEntity<UserPageRating> ratePageService(UserPageRatingDTO userRating, Long pageId){
+
+		//current logged-in user
+    	String username = SecurityContextHolder.getContext().getAuthentication().getName();
+    	User currentUser = userRepository.getUserByEmail(username);
+    	userRating.setUserId(currentUser.getId());
+		
+    	//validation to prevent multiple rating input and update rating if changed.
+    	UserPageRating savedRating = userPageRatingRepository.findByPageIdAndUserId(pageId, userRating.getUserId());
+    	if(savedRating != null) {
+    		if(savedRating.getRating().equals(userRating.getRating().toUpperCase())) {
+    			//rating will be deleted if the request have same rating(user can undo their rating)
+    			userPageRatingRepository.delete(savedRating);
+    			return ResponseEntity.status(HttpStatus.OK).body(savedRating);
+    		}else {
+    			//update rating
+    			savedRating.setRating(userRating.getRating().toUpperCase());
+    			userPageRatingRepository.save(savedRating);
+    			return ResponseEntity.status(HttpStatus.OK).body(savedRating);
+    		}	
+    	}else {
+    		UserPageRating newUserPageRating = new UserPageRating();
+    		newUserPageRating.setRating(userRating.getRating().toUpperCase());
+    		newUserPageRating.setUser(currentUser);
+    		
+    		//selected page to rate
+        	Optional<Page> page = pageRepository.findByIdAndActive(pageId, true);
+    		newUserPageRating.setPage(page.get());
+
+    		userPageRatingRepository.save(newUserPageRating);
+        	return ResponseEntity.status(HttpStatus.CREATED).body(newUserPageRating);
+    	}
+    }
+}
