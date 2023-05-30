7b8c5013d7f710c1e11694add152abfe26fbc4fc
Adrianne Marie Malonzo <malonzoandee@gmail.com>
2023-05-30 22:27:59 +0800

    refractor handling of errors

--- src/main/java/com/yondu/knowledgebase/services/ReviewService.java
+++ src/main/java/com/yondu/knowledgebase/services/ReviewService.java
@@ -7,9 +7,9 @@ import com.yondu.knowledgebase.DTO.permission.PermissionDTOMapper;
 import com.yondu.knowledgebase.DTO.review.ReviewDTO;
 import com.yondu.knowledgebase.DTO.review.ReviewDTOMapper;
 import com.yondu.knowledgebase.entities.*;
-import com.yondu.knowledgebase.exceptions.AlreadyExistException;
-import com.yondu.knowledgebase.exceptions.BadRequestException;
-import com.yondu.knowledgebase.exceptions.NotFoundException;
+import com.yondu.knowledgebase.exceptions.DuplicateResourceException;
+import com.yondu.knowledgebase.exceptions.RequestValidationException;
+import com.yondu.knowledgebase.exceptions.ResourceNotFoundException;
 import com.yondu.knowledgebase.repositories.PageVersionRepository;
 import com.yondu.knowledgebase.repositories.UserRepository;
 import org.springframework.security.core.Authentication;
@@ -47,7 +47,7 @@ public class ReviewService {
 
     public ReviewDTO.BaseResponse createReview(Long pageId, Long versionId) {
         PageVersion pageVersion = pageVersionRepository.findById(versionId)
-                .orElseThrow(() -> new NotFoundException("Page version not found"));
+                .orElseThrow(() -> new ResourceNotFoundException("Page version not found"));
 
         Review review = new Review();
         review.setPageVersion(pageVersion);
@@ -67,11 +67,11 @@ public class ReviewService {
     public ReviewDTO.UpdatedResponse updateReview(Long id, ReviewDTO.UpdateRequest request) {
 
         String email = SecurityContextHolder.getContext().getAuthentication().getName();
-        User currentUser = userRepository.findByEmail(email).orElseThrow(() -> new NotFoundException(String.format("User 'email' not found: %s", email)));
-        Review review = reviewRepository.findById(id).orElseThrow(() -> new NotFoundException(String.format("Review id not found: %s", id)));
+        User currentUser = userRepository.findByEmail(email).orElseThrow(() -> new ResourceNotFoundException(String.format("User 'email' not found: %s", email)));
+        Review review = reviewRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(String.format("Review id not found: %s", id)));
 
         if (review.getStatus().toUpperCase().contains("APPROVED") || review.getStatus().toUpperCase().contains("DISAPPROVE")) {
-            throw new BadRequestException("Content is already reviewed.");
+            throw new RequestValidationException("Content is already reviewed.");
         }
 
         if (request.status().toUpperCase().contains("APPROVED") || request.status().toUpperCase().contains("DISAPPROVE")) {
@@ -83,7 +83,7 @@ public class ReviewService {
             Review updatedReview = reviewRepository.save(review);
             return ReviewDTOMapper.mapToUpdatedResponse(updatedReview);
         } else {
-            throw new BadRequestException("Invalid Status, try APPROVED or DISAPPROVE");
+            throw new RequestValidationException("Invalid Status, try APPROVED or DISAPPROVE");
         }
 
     }
