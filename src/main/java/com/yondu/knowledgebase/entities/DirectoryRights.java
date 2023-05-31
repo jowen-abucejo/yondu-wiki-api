6faf366cb64771f044dfbbe0bd9b04faa898e9fd
mbaluyot <m.gwynethbaluyot@gmail.com>
2023-05-31 14:14:36 +0800

    added rights, pageRights, directoryRights entities

---
+++ src/main/java/com/yondu/knowledgebase/entities/DirectoryRights.java
@@ -0,0 +1,12 @@
+package com.yondu.knowledgebase.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class DirectoryRights extends Rights{
+    @ManyToOne
+    @JoinColumn(name = "directory_id")
+    private Directory directory;
+}
---
+++ src/main/java/com/yondu/knowledgebase/entities/PageRights.java
@@ -0,0 +1,12 @@
+package com.yondu.knowledgebase.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class PageRights extends Rights{
+    @ManyToOne
+    @JoinColumn(name = "page_id")
+    private Page page;
+}
