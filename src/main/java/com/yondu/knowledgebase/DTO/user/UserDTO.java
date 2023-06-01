787a8b91000044d8f2a76cd918de0eb48e4b1344
cediperez12 <kirisakinoshka@gmail.com>
2023-06-01 15:13:42 +0800

    added roles to /auth/login response

--- src/main/java/com/yondu/knowledgebase/DTO/user/UserDTO.java
+++ src/main/java/com/yondu/knowledgebase/DTO/user/UserDTO.java
@@ -1,10 +1,14 @@
 package com.yondu.knowledgebase.DTO.user;
 
+import com.yondu.knowledgebase.DTO.role.RoleDTO;
+
 import java.time.LocalDate;
+import java.util.Set;
 
 public class UserDTO {
     public record GeneralInfo(Long id, String email, String username, String password, String firstName, String lastName, String status, LocalDate createdAt) {}
     public record GeneralResponse(Long id, String email, String username, String firstName, String lastName, String status, LocalDate createdAt) {}
+    public record BaseWithRoles(Long id, String email , String username, String firstName, String lastName, String status, LocalDate createdAt, Set<RoleDTO> roles) {}
     public record BaseResponse(Long id, String email, String username, String firstName, String lastName, String status, LocalDate createdAt) {}
     public record LoginRequest(String email, String password) {}
     public record ShortResponse(Long id, String email, String username, String firstName, String lastName){}
