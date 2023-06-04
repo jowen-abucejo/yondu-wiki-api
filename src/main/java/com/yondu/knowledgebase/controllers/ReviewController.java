079cab9be788d3a8f46df8bfa460a18986790a9e
Adrianne Marie Malonzo <malonzoandee@gmail.com>
2023-06-04 21:50:47 +0800

    added 1 endpoint, fixed validation

--- src/main/java/com/yondu/knowledgebase/controllers/ReviewController.java
+++ src/main/java/com/yondu/knowledgebase/controllers/ReviewController.java
@@ -1,7 +1,9 @@
 package com.yondu.knowledgebase.controllers;
 
 import com.yondu.knowledgebase.DTO.ApiResponse;
+import com.yondu.knowledgebase.DTO.page.PaginatedResponse;
 import com.yondu.knowledgebase.DTO.review.ReviewDTO;
+import com.yondu.knowledgebase.entities.Review;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -26,41 +28,35 @@ public class ReviewController {
 
     @GetMapping("/reviews")
     public ResponseEntity<ApiResponse<List<ReviewDTO.BaseResponse>>> getAllReviews() {
-        try {
             return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(reviewService.getAllReviews(), "Success retrieving list of page reviews"));
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ApiResponse.error("Failed to retrieve list of reviews!"));
-        }
     }
 
 @PostMapping("/pages/{pageId}/versions/{versionId}/reviews")
     public ResponseEntity<ApiResponse<?>> createReview(@PathVariable("pageId") Long pageId, @PathVariable("versionId") Long versionId) {
-
-    try {
         Object data = reviewService.createReview(pageId, versionId);
         return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success(data, "Review submitted successfully"));
-    } catch (Exception e) {
-        System.out.println(e.getMessage());
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ApiResponse.error("Failed to create review!"));
-    }
+
 
 }
     @PutMapping("/reviews/update/{id}")
     @PreAuthorize("hasAuthority('CONTENT_APPROVAL')")
     public ResponseEntity<ApiResponse<?>> updateReview(@PathVariable("id") Long id, @RequestBody ReviewDTO.UpdateRequest request) {
-        try {
             Object data = reviewService.updateReview(id, request);
             return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(data, "Review updated successfully"));
-
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ApiResponse.error("Failed to update Review"));
-        }
-
     }
 
     @GetMapping("/reviews/{id}")
     public ResponseEntity<ApiResponse<ReviewDTO.BaseResponse>> getReviewById(@PathVariable Long id){
         ReviewDTO.BaseResponse review = reviewService.getReview(id);
+
         return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(review, "Review with id: " + id + " found"));
+    }
+
+    @GetMapping("reviews/search")
+    public ResponseEntity<?> getReviewByStatus(@RequestParam(defaultValue = "PENDING") String status, @RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue = "20") int size){
+        PaginatedResponse<ReviewDTO.BaseResponse> review = reviewService.getAllReviewsByStatus(status,page,size);
+
+        ApiResponse apiResponse = ApiResponse.success(review, "Retrieved reviews successfully");
+        return ResponseEntity.ok(apiResponse);
     }
 }
