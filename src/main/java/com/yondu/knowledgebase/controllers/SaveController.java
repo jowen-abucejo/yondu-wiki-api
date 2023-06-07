ca77fea259074a2f9033828ef65d8f160d271516
Adrianne Marie Malonzo <malonzoandee@gmail.com>
2023-06-07 18:08:22 +0800

    added create - not yet furnished

--- src/main/java/com/yondu/knowledgebase/controllers/SaveController.java
+++ src/main/java/com/yondu/knowledgebase/controllers/SaveController.java
@@ -1,7 +1,32 @@
 package com.yondu.knowledgebase.controllers;
 
+import com.yondu.knowledgebase.DTO.ApiResponse;
+import com.yondu.knowledgebase.DTO.save.SaveDTO;
+import com.yondu.knowledgebase.services.SaveService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("save")
 public class SaveController {
+
+    private final SaveService saveService;
+
+    @Autowired
+    public SaveController (SaveService saveService) {
+        this.saveService = saveService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<ApiResponse<SaveDTO.BaseResponse>> createSavedPosts (@RequestBody SaveDTO.BaseRequest save) {
+        SaveDTO.BaseResponse saved = saveService.createSaved(save);
+
+        ApiResponse apiResponse = ApiResponse.success(saved, "success");
+        return ResponseEntity.status(HttpStatus.CREATED).body(apiResponse);
+    }
 }
--- src/main/java/com/yondu/knowledgebase/services/SaveService.java
+++ src/main/java/com/yondu/knowledgebase/services/SaveService.java
@@ -3,6 +3,10 @@ package com.yondu.knowledgebase.services;
 import com.yondu.knowledgebase.DTO.save.SaveDTO;
 import com.yondu.knowledgebase.DTO.save.SaveDTOMapper;
 import com.yondu.knowledgebase.entities.Save;
+import com.yondu.knowledgebase.entities.User;
+import com.yondu.knowledgebase.repositories.SaveRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -10,5 +14,18 @@ import java.time.LocalDateTime;
 @Service
 public class SaveService {
 
+    @Autowired
+    private SaveRepository saveRepository;
 
+public SaveDTO.BaseResponse createSaved(SaveDTO.BaseRequest saves) {
+    User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+    Save saved = SaveDTOMapper.mapBaseToEntity(saves);
+    saved.setDateCreated(LocalDateTime.now());
+    saved.setAuthor(user);
+
+
+    SaveDTO.BaseResponse saveRes = SaveDTOMapper.mapEntityToBaseResponse(saveRepository.save(saved));
+    return saveRes;
+}
 }
