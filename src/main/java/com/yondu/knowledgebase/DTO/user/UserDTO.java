aa785c67a7fc8c33ebcea4bf82bf5ae4754a6613
cediperez12 <kirisakinoshka@gmail.com>
2023-06-14 17:07:35 +0800

    refactored user request

--- src/main/java/com/yondu/knowledgebase/DTO/user/UserDTO.java
+++ src/main/java/com/yondu/knowledgebase/DTO/user/UserDTO.java
@@ -1,5 +1,6 @@
 package com.yondu.knowledgebase.DTO.user;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.yondu.knowledgebase.DTO.role.RoleDTO;
 
 import java.time.LocalDate;
@@ -14,7 +15,7 @@ public class UserDTO {
     public record ShortResponse(Long id, String email, String username, String firstName, String lastName){}
     public record ShortRequest(String email){}
 
-    public record WithRolesRequest(Long id, String email, String password, String profilePhoto, String position, String username, String firstName, String lastName, String status, LocalDate createdAt, Set<RoleDTO> roles) {}
+    public record WithRolesRequest(Long id, String email, String password, @JsonProperty("profile_photo") String profilePhoto, String position, String username, @JsonProperty("first_name") String firstName, @JsonProperty("last_name") String lastName, String status, @JsonProperty("created_at") LocalDate createdAt, Set<RoleDTO> roles) {}
     public record WithRolesResponse(Long id, String email , String username, String profilePhoto, String position, String firstName, String lastName, String status, LocalDate createdAt, Set<RoleDTO> roles) {}
 
 
