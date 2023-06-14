8be691ff150d210c95d7d2dd78a53ab0812730c7
nikkoberse <nikko.berse.23@gmail.com>
2023-06-14 13:49:26 +0800

    Added JsonProperty to comment request to transform it to snake case

--- src/main/java/com/yondu/knowledgebase/DTO/comment/CommentDTO.java
+++ src/main/java/com/yondu/knowledgebase/DTO/comment/CommentDTO.java
@@ -1,8 +1,6 @@
 package com.yondu.knowledgebase.DTO.comment;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.yondu.knowledgebase.DTO.user.UserDTO;
 
 import java.time.LocalDateTime;
@@ -10,12 +8,8 @@ import java.util.List;
 import java.util.Set;
 
 public class CommentDTO {
-    @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
     public record BaseRequest(String comment, @JsonProperty("entity_id") Long entityId, @JsonProperty("entity_type") String entityType, @JsonProperty("comment_mentions") Long[] commentMentions) {}
-    @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
     public record BaseComment (Long id, LocalDateTime dataCreated, String comment, UserDTO.GeneralResponse user, Long parentCommentId, Long entityId, String entityType, boolean allowReply, Set<UserDTO.GeneralResponse> commentMentions){}
-    @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
     public record BaseResponse (CommentDTO.BaseComment comment, Long totalReplies, List<CommentDTO.BaseComment> replies){}
-    @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
     public record CountResponse (Long entityId, String entityType, Long totalComment){}
 }
