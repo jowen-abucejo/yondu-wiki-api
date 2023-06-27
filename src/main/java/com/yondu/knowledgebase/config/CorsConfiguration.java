e8b50c556a79f8891106ea5763efe8f48595f6c2
cediperez12 <kirisakinoshka@gmail.com>
2023-06-27 20:09:01 +0800

    implemented origins for react

--- src/main/java/com/yondu/knowledgebase/config/CorsConfiguration.java
+++ src/main/java/com/yondu/knowledgebase/config/CorsConfiguration.java
@@ -11,7 +11,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "https://ykb-react.netlify.app/")
+                .allowedOrigins("http://localhost:5173", "https://ykb-react.netlify.app")
                 .allowedHeaders("*")
                 .allowedMethods("*")
                 .allowCredentials(true);
