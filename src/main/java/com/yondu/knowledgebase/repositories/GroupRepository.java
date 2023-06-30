64b4746fe5885e435bcc8f9b8ecb5d0bf633d0f9
Kalamari <sueltokarla@gmail.com>
2023-06-30 09:39:58 +0800

    added new controller (paginated groups with search)

--- src/main/java/com/yondu/knowledgebase/repositories/GroupRepository.java
+++ src/main/java/com/yondu/knowledgebase/repositories/GroupRepository.java
@@ -1,20 +1,26 @@
 package com.yondu.knowledgebase.repositories;
 
 import com.yondu.knowledgebase.entities.Group;
-import com.yondu.knowledgebase.entities.Rights;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-import java.util.Set;
 
 @Repository
 public interface GroupRepository extends JpaRepository<Group, Long> {
     boolean existsByName(String name);
-    @Query("SELECT g.rights FROM cluster g WHERE g.id = :userGroupId ")
-    Set<Rights> findRightsById(Long userGroupId);
-
     @Query("SELECT g FROM cluster g WHERE g.id = :userGroupId AND g.isActive = :isActive ")
     Optional<Group> findByIdAndIsActive(Long userGroupId, Boolean isActive);
+
+    @Query("SELECT g FROM cluster g WHERE g.name LIKE %:searchKey% AND g.isActive = :isActive ")
+    Page<Group> findAllByNameAndStatus(String searchKey, boolean isActive, PageRequest pageRequest);
+
+    @Query("SELECT g FROM cluster g WHERE g.isActive = :isActive ")
+    Page<Group> findAllByStatus(boolean isActive, PageRequest pageRequest);
+
+    @Query("SELECT g FROM cluster g WHERE g.name  LIKE %:searchKey% ")
+    Page<Group> findAllByName(String searchKey, PageRequest pageRequest);
 }
