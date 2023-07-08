758e8394114ccf0575cb3d68d38552324059602c
Jowen Abucejo <jabucejo@yondu.com>
2023-07-08 18:16:51 +0800

    Add endpoints to retrieve ids of  user permissions for a directory or page

--- src/main/java/com/yondu/knowledgebase/controllers/PermissionController.java
+++ src/main/java/com/yondu/knowledgebase/controllers/PermissionController.java
@@ -2,7 +2,6 @@ package com.yondu.knowledgebase.controllers;
 
 import com.yondu.knowledgebase.DTO.ApiResponse;
 import com.yondu.knowledgebase.DTO.permission.PermissionDTO;
-import com.yondu.knowledgebase.exceptions.ResourceNotFoundException;
 import com.yondu.knowledgebase.services.PermissionService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Set;
 
 @RestController
 public class PermissionController {
@@ -20,15 +20,32 @@ public class PermissionController {
     public PermissionController(PermissionService permissionService) {
         this.permissionService = permissionService;
     }
+
     @GetMapping("/permissions")
-    public ResponseEntity<ApiResponse<List<PermissionDTO.BaseResponse>>> getAllPermission(){
-        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(permissionService.getAllPermission(), "Success retrieving list of permissions"));
+    public ResponseEntity<ApiResponse<List<PermissionDTO.BaseResponse>>> getAllPermission() {
+        return ResponseEntity.status(HttpStatus.OK).body(
+                ApiResponse.success(permissionService.getAllPermission(), "Success retrieving list of permissions"));
     }
 
     @GetMapping("/permissions/{id}")
-    public ResponseEntity<ApiResponse<PermissionDTO.BaseResponse>> getPermissionById(@PathVariable Long id){
+    public ResponseEntity<ApiResponse<PermissionDTO.BaseResponse>> getPermissionById(@PathVariable Long id) {
         PermissionDTO.BaseResponse permission = permissionService.getPermission(id);
-        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(permission, "Permission with id: " + id + " found"));
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ApiResponse.success(permission, "Permission with id: " + id + " found"));
+    }
+
+    @GetMapping(path = "user-permissions/directories/{directoryId}")
+    public ResponseEntity<ApiResponse<Set<Long>>> getAllUserPermissionByDirectoryId(@PathVariable Long directoryId) {
+        Set<Long> permissions = permissionService.getAllUserPermissionByDirectoryId(directoryId);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ApiResponse.success(permissions, "Success retrieving list of directory permissions"));
+    }
+
+    @GetMapping(path = "user-permissions/pages/{pageId}")
+    public ResponseEntity<ApiResponse<Set<Long>>> getAllUserPermissionByPageId(@PathVariable Long pageId) {
+        Set<Long> permissions = permissionService.getAllUserPermissionByPageId(pageId);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ApiResponse.success(permissions, "Success retrieving list of directory permissions"));
     }
 
 }
