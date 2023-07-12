3f31ea9502cb8a4828549f67013c547fc510aa73
cediperez12 <kirisakinoshka@gmail.com>
2023-07-12 09:47:15 +0800

    created some new endpoints

--- src/main/java/com/yondu/knowledgebase/controllers/PageRightsController.java
+++ src/main/java/com/yondu/knowledgebase/controllers/PageRightsController.java
@@ -4,10 +4,13 @@ package com.yondu.knowledgebase.controllers;
 import com.yondu.knowledgebase.DTO.ApiResponse;
 import com.yondu.knowledgebase.DTO.group.GroupDTO;
 import com.yondu.knowledgebase.DTO.page_rights.user_access.PageRightsDTO;
+import com.yondu.knowledgebase.DTO.permission.PermissionDTO;
 import com.yondu.knowledgebase.services.PageRightsService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
 
 
 @RestController
@@ -78,6 +81,47 @@ public class PageRightsController {
     public ResponseEntity<ApiResponse<PageRightsDTO.GetUserPageRightResponse>> getRightsOfUser(@PathVariable Long userId ){
         PageRightsDTO.GetUserPageRightResponse data = pageRightsService.getPageRightsOfUser(userId);
         return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(data, "All Pages the User can access has been fetched successfully"));
+    }
+
+    /**
+     * Gets the user's rights based
+     * on the page.
+     *
+     * @param pageId
+     * @return
+     */
+    @GetMapping("/{pageId}/user/rights")
+    public ResponseEntity<ApiResponse<PageRightsDTO.GetPageRightResponse>> getPageRightsOfUserToPage(@PathVariable Long pageId) {
+        PageRightsDTO.GetPageRightResponse response = pageRightsService.getPageRightsOfUserToPage(pageId);
+        return ResponseEntity.ok().body(ApiResponse.success(response, "Successfully fetched user's rights to page."));
+    }
+
+    /**
+     * Checks if the user has
+     * the set of rights to page.
+     */
+    @PostMapping("/{pageId}/user/has-rights")
+    public ResponseEntity checkUserHasRights(@PathVariable Long pageId, @RequestBody Set<PermissionDTO.BaseRequest> permissions) {
+        pageRightsService.checkUserHasRights(pageId, permissions);
+        return ResponseEntity.ok().build();
+    }
+
+    /**
+     *
+     */
+    @PostMapping("/{pageId}/users/{userId}/has-rights")
+    public ResponseEntity checkIfOtherUserHasRights(@PathVariable Long pageId, @PathVariable Long userId, @RequestBody Set<PermissionDTO.BaseRequest> permissions) {
+        pageRightsService.checkIfOtherUserHasRights(pageId, userId, permissions);
+        return ResponseEntity.ok().build();
+    }
+
+    /**
+     *
+     */
+    @PostMapping("/{pageId}/groups/{groupId}/has-rights")
+    public ResponseEntity checkIfGroupHasRights(@PathVariable Long pageId, @PathVariable Long groupId, @RequestBody Set<PermissionDTO.BaseRequest> permissions) {
+        pageRightsService.checkIfGroupHasRights(pageId, groupId, permissions);
+        return ResponseEntity.ok().build();
     }
 
 
