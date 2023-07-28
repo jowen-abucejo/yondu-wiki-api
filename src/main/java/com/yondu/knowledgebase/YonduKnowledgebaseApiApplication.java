f26596f2f53313de98e876c01ade1c786b268932
Jowen Abucejo <jabucejo@yondu.com>
2023-07-28 10:50:19 +0800

    Remove posts and pages from category dto
    Refactor chatbase service implementation
    
    Add application properties for second chatbot

--- src/main/java/com/yondu/knowledgebase/YonduKnowledgebaseApiApplication.java
+++ src/main/java/com/yondu/knowledgebase/YonduKnowledgebaseApiApplication.java
@@ -2,7 +2,6 @@ package com.yondu.knowledgebase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
