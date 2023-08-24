230dacbe3165b70abdad4f66a9244bac4ec691b8
Jowen Abucejo <jabucejo@yondu.com>
2023-08-24 16:01:28 +0800

    Fix:Ensure directory group access table are always joined in userHasPagePermission and userHasDirectoryPermission native queries

--- src/main/java/com/yondu/knowledgebase/repositories/UserRepository.java
+++ src/main/java/com/yondu/knowledgebase/repositories/UserRepository.java
@@ -48,10 +48,11 @@ public interface UserRepository extends JpaRepository<User, Long> {
                         FROM page p
                         WHERE
                         p.id = :pageId
-                    ) p20 LEFT JOIN
+                    ) p20 CROSS JOIN
+                    (SELECT id FROM users WHERE id=:userId) u20 LEFT JOIN
                     (SELECT * FROM user_page_access WHERE user_id=:userId AND permission_id IN (SELECT DISTINCT rp.permission_id FROM user_role ur LEFT JOIN role_permission rp ON rp.role_id=ur.role_id WHERE ur.user_id=:userId)) upa20 ON p20.id = upa20.page_id LEFT JOIN
                     (SELECT * FROM directory_user_access WHERE user_id=:userId AND permission_id IN (SELECT DISTINCT rp.permission_id FROM user_role ur LEFT JOIN role_permission rp ON rp.role_id=ur.role_id WHERE ur.user_id=:userId)) dua20 ON p20.directory_id = dua20.directory_id LEFT JOIN
-                    (SELECT * FROM group_users WHERE user_id=:userId) gu20 ON dua20.user_id = gu20.user_id LEFT JOIN
+                    (SELECT * FROM group_users WHERE user_id=:userId) gu20 ON u20.id = gu20.user_id LEFT JOIN
                     (SELECT id, is_active FROM cluster  WHERE is_active) ct20 ON ct20.id = gu20.group_id LEFT JOIN
                     group_permissions gp20 ON gp20.group_id = ct20.id LEFT JOIN
                     group_page_access gpa20 ON (ct20.id = gpa20.group_id AND gpa20.page_id=p20.id AND gp20.permission_id=gpa20.permission_id) LEFT JOIN
@@ -72,18 +73,19 @@ public interface UserRepository extends JpaRepository<User, Long> {
                 SELECT
                     pr20.name
                 FROM
-                    (SELECT * FROM directory_user_access WHERE user_id=:userId AND directory_id = :directoryId AND permission_id IN (SELECT DISTINCT rp.permission_id FROM user_role ur LEFT JOIN role_permission rp ON rp.role_id=ur.role_id WHERE ur.user_id=:userId)) dua20 LEFT JOIN
-                    (SELECT * FROM group_users WHERE user_id=:userId) gu20 ON dua20.user_id = gu20.user_id LEFT JOIN
-                    (SELECT id, is_active FROM cluster WHERE is_active) ct20 ON ct20.id = gu20.group_id LEFT JOIN
+                    (SELECT id FROM users WHERE id=:userId) u20 LEFT JOIN
+                    (SELECT * FROM directory_user_access WHERE directory_id=:directoryId AND user_id=:userId AND permission_id IN (SELECT DISTINCT rp.permission_id FROM user_role ur LEFT JOIN role_permission rp ON rp.role_id=ur.role_id WHERE ur.user_id=:userId)) dua20 ON u20.id = dua20.user_id LEFT JOIN
+                    (SELECT * FROM group_users WHERE user_id=:userId) gu20 ON u20.id = gu20.user_id LEFT JOIN
+                    (SELECT id, is_active FROM cluster  WHERE is_active) ct20 ON ct20.id = gu20.group_id LEFT JOIN
                     group_permissions gp20 ON gp20.group_id = ct20.id LEFT JOIN
-                    directory_group_access dga20 ON (ct20.id = dga20.group_id AND dga20.directory_id=dua20.directory_id AND gp20.permission_id=dga20.permission_id) LEFT JOIN
+                    (SELECT * FROM directory_group_access WHERE directory_id=:directoryId) dga20 ON (ct20.id = dga20.group_id AND gp20.permission_id=dga20.permission_id) LEFT JOIN
                     permission pr20 ON (
                         (pr20.id = dua20.permission_id)
                         OR (pr20.id = dga20.permission_id)
                     )
                 WHERE pr20.name=:permission
                 GROUP BY pr20.name) isGranted
-                                                                """)
+                                                                    """)
     public Long userHasDirectoryPermission(Long userId, Long directoryId, String permission);
 
     @Query("SELECT u FROM users u JOIN u.role r WHERE (CONCAT(u.firstName, ' ', u.lastName) LIKE %:searchKey% OR u.email LIKE %:searchKey%) AND u.status = :statusFilter AND r.roleName = :roleFilter")
